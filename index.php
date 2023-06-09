c21eed301d01db057bf20699b5732cb6834ad49b
jaimereyesr <jaime.reyesr22@gmail.com>
2023-06-08 22:05:22 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KOUTILAB</title>
+    <link rel="shortcut icon" href="img/lgk.png">
+    <link rel="stylesheet" href="css/principal.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="js/bootstrap.min.js.js">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.plyr.io/3.7.3/plyr.css" />
+    <link rel="stylesheet" href="Comfortaa/Comfortaa-VariableFont_wght.ttf">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
+    <script>
+        $(document).ready(function() {
+            $('#icon').click(function() {
+                $('ul').toggleClass('show');
+            });
+        });
+    </script>
+
+</head>
+
+<body>
+    <nav>
+        <a href="index.html">
+            <img src="img/koutilab.png" alt="" class="logo">
+        </a>
+        <ul>
+            <li> <a href="#Que">¿Qué es?</a></li>
+            <li><a href="#Funcion">¿Cómo funciona?</a></li>
+            <li><a href="#Rutas">Rutas de aprendizaje</a></li>
+            <li><a href="#Niveles">Niveles escolares</a></li>
+            <li><a class="Rg" href="registro/index.php">Registrarse</a></li>
+            <li>
+                <a class="is" href="login.php"></i><i class="fas fa-user-alt" style="color: #fff"></i> Iniciar sesión</a>
+            </li>
+        </ul>
+    </nav> <br><br><br>
+    <section id="Que" class="container-fluid fon">
+        <div class="row">
+            <div class="col"><br><br><br><br> <br>
+                <img src="img/2023 koutilab web imagenes-06.png" class="img-fluid">
+            </div>
+            <div class="col">
+                <img src="img/2023 koutilab web imagenes-05.png" class="img-fluid3 ima ">
+                <p style="font-size: 25px " class="g"> ¿Ya enseñas programación <br> en tu escuela?</p>
+                <p class="pl" style="font-size: 25px">Plataforma SIN COSTO para <br> centros educativos</p>
+            </div>
+        </div><br><br>
+        <p style="font-size: 30px" class="g">Conoce Koutilab la <span style="font-size: 30px">herramientas autodidacta ideal
+            </span>para que tus alumnos <br> descubran <span style="font-size: 30px">el mundo de la programación</span>
+        </p>
+    </section>
+
+    <section id="Funcion" class="container-fluid "><br><br><br><br>
+        <div class="row">
+            <div><br><br> <br><br><br>
+                <img src="img/2023 koutilab web imagenes-07.png" alt="" class="img-fluid3">
+                <p class="g ris" style="font-size: 30px; text-align: center; margin-left: 20%;" aling="center">
+                    Utilizamos una ruta de aprendizaje con cápsulas de coding <br> diferentes para guiar a cada alumno en su aprendizaje de CODING.</p>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="fonV">
+            <video id="player" playsinline controls>
+                <source src="vid/DEMO1.mp4" type="video/mp4" />
+            </video>
+        </div>
+    </section>
+
+    <section id="Rutas" class="container-fluid c"><br><br>
+        <p style="font-size: 30px " class="r">Esta ruta consiste en:</p><br><br><br><br>
+
+        <div class="row">
+            <div class="col r">
+                <fieldset class="ru">
+                    <img src="img/Copia de elementos kouti web-06.png" alt="" class="elemento " height="100px" width="100px">
+                    <div id="el1"><br><br>
+                        <h4><b>Cápsulas de contenidos</b></h4>
+                        <p>
+                            Todo lo necesario para aprender los conceptos.
+                        </p>
+                    </div>
+                </fieldset><br>
+                <fieldset class="ru">
+                    <img src="img/Copia de elementos kouti web-07.png" alt="" class="elemento" height="100px" width="100px">
+                    <div id="el2"><br><br>
+                        <h4><b>Gamificación</b></h4>
+                        <p>
+                            Mantén a tus alumnos motivados con insignias.
+                        </p>
+                    </div>
+                </fieldset><br>
+                <fieldset class="ru">
+                    <img src="img/Copia de elementos kouti web-08.png" alt="" class="elemento" height="100px" width="100px">
+                    <div id="el3"><br>
+                        <h4><b>FabLabs</b></h4>
+                        <p>
+                            La práctica hace al maestro. Pondrán en práctica <br> los conocimientos con retos a resolver.
+                        </p>
+                    </div>
+
+                </fieldset>
+            </div>
+            <div class="col">
+                <img class="ider" src="img/2023 koutilab web imagenes-10.png" style="margin-left: 100px;"><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            </div>
+    </section>
+    <section>
+        <div class="row">
+            <div class="tit col">
+                <p><span style="font-size: 30px ">Usamos inteligencia artificial <br> para medir el desempeño de <br>cada alumno </span> </p>
+                <center>
+                    <p class=" ra sec" style="font-size: 30px ">Asegura el aprendizaje de tus <br>alumnos usando métricas de forma <br>personalizada
+                    </p>
+                </center>
+
+            </div><br><br><br><br>
+            <div class="col">
+                <img class="imge" src="img/Grafica.png">
+            </div>
+        </div><br><br><br>
+    </section><br><br>
+    <div class="container-fluid" id="Fonden">
+        <p><span style="font-size: 45px "> <b> ¿Qué aprenderán en <img src="img/koutilab.png" alt="" height="55px" width="165px">?</b></span></p>
+        <section id="Niveles" class="row fon2 ">
+            <div class="col ri">
+                <fieldset class="nivel">
+                    <legend>
+                        <center><img src="img/2023 koutilab web imagenes-13.png" alt="" class="img-fluid2"></center>
+                    </legend>
+
+                    <p class="g">¡Al terminar su <br> primaria tus <br> alumnos <br> desarrollan y <br> programan <br> increíbles <br> videojuegos
+                    </p>
+                </fieldset>
+            </div>
+            <div class="col">
+                <fieldset class="nivel sec">
+                    <legend>
+                        <center><img src="img/2023 koutilab web imagenes-14.png" alt="" class="img-fluid2"></center>
+                    </legend>
+
+                    <p class="g">¡Desarrollar páginas <br> web, nunca fue tan <br> fácil y divertido!</p>
+                    <center><img src="img/3003511.png" alt="" class="img-fluid2"></center>
+                </fieldset>
+            </div>
+            <div class="col ">
+                <fieldset class="nivel">
+                    <legend>
+                        <center><img src="img/2023 koutilab web imagenes-15.png" alt="" class="img-fluid2"></center>
+                    </legend </fieldset>
+                    <p class="g">Incentiva a tus <br> alumnos <br> aprendiendo <br> lenguajes de <br> programación y <br> algoritmos
+                    </p>
+            </div>
+
+        </section>
+    </div>
+    <footer>
+        <div class="content">
+            <div class="left box">
+                <div class="upper">
+                    <div class="topic">Sobre nosotros</div>
+                    <p>Somos una plataforma autodidacta ideal para que nuestros usuarios descubran el mundo de la programación.</p>
+                </div>
+                <div class="lower">
+                    <div class="topic">Contacto</div>
+                    <div class="phone">
+                        <a href="#"><i class="fas fa-phone-volume"></i>2228279092</a>
+                    </div>
+                    <div class="email">
+                        <a href="#"><i class="fas fa-envelope"></i>info@koutilab.com</a>
+                    </div>
+                </div>
+            </div>
+            <div class="middle box">
+                <div class="topic">Otros servicios</div>
+                <div><a href="https://aerobotplanet.com/">Aerobot</a></div>
+                <div><a href="#"></a></div>
+                <div><a href="#">Aviso de privacidad</a></div>
+                <div><a href="#">Términos y condiciones</a></div>
+            </div>
+            <div class="right box">
+                <div class="topic">Registrate</div>
+                <form action="#">
+                    <div class="wrap">
+                        <button class="button">Registrarse</button>
+                    </div>
+                    <br><br>
+                    <div class="">
+                        <button class="glow-on-hover"><a href="https://www.facebook.com/AerobotPlanet/"><i class="fab fa-facebook-f"></i></a></button>
+                        <button class="glow-on-hover"><a href="https://www.instagram.com/aerobotplanet_oficial/"><i class="fab fa-instagram"></i></a></button>
+                        <button class="glow-on-hover"><a href="https://www.linkedin.com/company/aerobotplanet"><i class="fab fa-linkedin-in"></i></a></button>
+                        <button class="glow-on-hover"><a href="https://api.whatsapp.com/send?phone=5219712870361"><i class="fab fa-whatsapp"></i></a></button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="bottom">
+            <p>© Copyright <a href="#"><b>Aerobot</b></a> All Rights Reserved</p>
+        </div>
+    </footer>
+
+    <script src="https://cdn.plyr.io/3.7.3/plyr.polyfilled.js"></script>
+</body>
+
+</html>
