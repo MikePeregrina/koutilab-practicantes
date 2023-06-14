2011f01edd88a3598d17044d6be2781d85b6b9a9
RafaelSoledad02 <wwrafasc1@gmail.com>
2023-06-13 19:35:40 -0600

    Capsulas de juego PWB todos los niveles

---
+++ alumno/universidad/cursos/programacion-web/basico/capsulas/contenido/juegos/cjcss3.php
@@ -0,0 +1,181 @@
+<?php
+session_start();
+$id_user = $_SESSION['id_alumno_universidad'];
+if (empty($_SESSION['active']) || empty($_SESSION['id_alumno_universidad'])) {
+	header('location: ../../../../../../../../acciones/cerrarsesion.php');
+}
+include "../../../../../../../../acciones/conexion.php";
+$id_user = $_SESSION['id_alumno_universidad'];
+$permiso = "capsula34";
+$sql = mysqli_query($conexion, "SELECT c.*, d.* FROM capsulas_universidad c INNER JOIN detalle_capsulas_universidad d ON c.id_capsula = d.id_capsula WHERE d.id_alumno = $id_user AND c.nombre = '$permiso' AND d.id_curso = 1");
+$existe = mysqli_fetch_all($sql);
+if (empty($existe) && $id_user != 1) {
+	header("Location: ../../../../basico/capsulas/acciones/capsulas.php");
+}
+
+//Verificar si ya se tiene permiso y no dar puntos de más
+$permiso_intento = 35;
+$sql_permisos = mysqli_query($conexion, "SELECT * FROM detalle_capsulas_universidad WHERE id_capsula = $permiso_intento AND id_alumno = '$id_user' AND id_curso = 1");
+$result_sql_permisos = mysqli_num_rows($sql_permisos);
+//Script para poder ver cuantos intentos lleva el alumno en la capsula y mostrar cuantos puntos gano dependiendo los intentos
+
+//Contar total de intentos
+$consultaIntentos = mysqli_query($conexion, "SELECT intentos FROM detalle_intentos_universidad WHERE id_capsula = $permiso_intento AND id_alumno = $id_user AND id_curso = 1");
+$resultadoIntentos = mysqli_fetch_assoc($consultaIntentos);
+if (isset($resultadoIntentos['intentos'])) {
+	$totalIntentos = $resultadoIntentos['intentos'];
+	if ($totalIntentos == 2 && $result_sql_permisos == 0) {
+		$puntosGanados = 8;
+	} else if ($totalIntentos == 3 && $result_sql_permisos == 0) {
+		$puntosGanados = 6;
+	} else if ($totalIntentos > 3 && $result_sql_permisos == 0) {
+		$puntosGanados = 0;
+	} else {
+		$puntosGanados = 0;
+	}
+} else {
+	$puntosGanados = 10;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/css-juegos/columnas.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <title>KOUTILAB</title>
+</head>
+
+<body onload="iniciarTiempo()">
+    <!-- Titulo general del juego -->
+    <div class="titulo-gen">
+        <h1 class="titulo"><b>RELACIONA LAS COLUMNAS</b></h1>
+    </div>
+
+    <!-- Timer -->
+    <div class="timer" id="timer">
+        <b>Tiempo: <br>
+            <p id="tiempo" style="margin: 0 0 0 0;"></p>
+        </b>
+    </div>
+
+    <!-- Contenedor principal -->
+    <div class="contenido">
+        <!-- Boton para regresar -->
+        <a href="../../../../../../rutas/ruta-pw-b.php"><button style="float: left; position: absolute; margin: 10px 0 0 10px;" class="btn-b"
+                id="btn-cerrar-modalV">
+                <i class="fas fa-reply"></i></button>
+        </a>
+
+        <!-- Titulo secundario -->
+        <h4 class="titulo"><b>Selecciona una palabra de lado izquierdo y relacionala con una del lado derecho</b></h4>
+
+        <br>
+        <!-- contenido del juego -->
+        <div class="container-all">
+            <!-- Columna de lado izquierdo -->
+            <div class="left-column">
+                <!-- opciones estas son las principales -->
+                <div class="word-box" id="root">:root{ }</div>                
+                <div class="word-box" id="menu">.menu-link{ }</div>
+                <div class="word-box" id="nombre">--nombre:" ";</div>
+                <div class="word-box" id="titulo">.titulo{ }</div>                
+                <div class="word-box" id="variable">variable</div>
+            </div>
+            <!-- Mapeo donde se trazan las lineas -->
+            <canvas id="canvas"> </canvas>
+
+            <!-- columna de lado derecho -->
+            <div class="right-column">
+                <!-- Respuestas -->
+                <div class="word-box" id="color" onclick="checkAnswer('color')">Color:" ";</div> 
+                <div class="word-box" id="locales" onclick="checkAnswer('locales')">Locales</div> 
+                <div class="word-box" id="datos" onclick="checkAnswer('datos')">Datos</div> 
+                <div class="word-box" id="valor" onclick="checkAnswer('valor')">Valor</div>
+                <div class="word-box" id="globales" onclick="checkAnswer('globales')">Globales</div>             
+                 
+            </div>
+        </div>
+
+        <!-- boton de verificar respuestas -->
+        <button class="verificar">Comprobar respuestas</button>
+    </div>
+
+    <!-- Linkeamos un documento donde tenemos todo lo relacionado a la relacion de columnas -->
+    <script src="../../js/seleccionador-v.js"></script>
+
+    <script>
+        //Contador de tiempo en segundos, si se acaba el tiempo sale alerta
+        var segundos = 240;
+
+        let puntos = 0;
+
+        function iniciarTiempo() {
+            document.getElementById('tiempo').innerHTML = segundos + " segundos";
+            if (segundos <= 60) {
+                    var div = document.getElementById("timer");
+                    div.style.cssText = " animation-name: animation1; animation-duration: 0.5s; background-color: #c42c2caf; border-color: #c42c2c;";
+                }
+                if (segundos <= 30) {
+                    var div = document.getElementById("timer");
+                    div.style.cssText = "animation-name: animation2; animation-duration: 0.5s; background-color: #c42c2caf; border-color: #c42c2c;";
+                }
+                if (segundos <= 10) {
+                    var div = document.getElementById("timer");
+                    div.style.cssText = "animation-name: animation2; animation-duration: 0.5s; background-color: #c42c2caf; border-color: #c42c2c;";
+                }
+            if (segundos == 0) {
+                var xmlhttp = new XMLHttpRequest();
+				var param = "score=" + 0 + "&validar=" + 'incorrecto' + "&permiso=" + 35 + "&id_curso=" + 1; //cancatenation
+				xmlhttp.open("POST", "../../acciones/insertar_pd35.php", true);
+				xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+				xmlhttp.send(param);
+                Swal.fire({
+                    title: 'Oops...',
+                    text: '¡Tiempo Agotado! Vuelve a intentarlo',
+                    imageUrl: "../../img/img_juegos/loop.gif",
+                    imageHeight: 350,
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location.reload();
+                    }
+                });
+            } else {
+                segundos--;
+                setTimeout("iniciarTiempo()", 1000);
+            }
+        }
+
+        // //Alerta muestra de que el juego fue completado
+        // function alertExcelent() {
+        //     Swal.fire({
+        //         title: 'Excelente',
+        //         text: '¡Buen trabajo!',
+        //         imageUrl: "img/Thumbs-Up.gif",
+        //         imageHeight: 350,
+        //         backdrop: `
+		// 				rgba(0,143,255,0.6)
+		// 				url("img/fondo.gif")`,
+        //         confirmButtonColor: '#a14cd9',
+        //         confirmButtonText: '¡Genial!',
+        //     }).then((result) => {
+        //         if (result.isConfirmed) {
+        //             window.location.reload();
+        //         }
+        //     });
+
+        // }
+
+
+    </script>
+</body>
+
+</html>
