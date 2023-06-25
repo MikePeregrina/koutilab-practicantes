af62c880bae0fc40c2eec5a498504dc30433a3fc
jaimereyesr <jaime.reyesr22@gmail.com>
2023-06-25 13:35:49 -0600

    Archivo de conexión

--- acciones/conexion.php
+++ acciones/conexion.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $database = "aerobotp_beta";
 $username = "root";
-$password = "tiburon2014";
+$password = "";
 // Create connection
 $conexion = mysqli_connect($servername, $username, $password, $database);
 // Check connection
