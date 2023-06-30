ae7d06c08088c0b9e7f7fad154edc9269deec620
RafaelSoledad02 <wwrafasc1@gmail.com>
2023-06-29 20:21:53 -0600

    correcciones

--- alumno/primaria/cursos/python/basico/capsulas/contenido/juegos/cjp1-2.php
+++ alumno/primaria/cursos/python/basico/capsulas/contenido/juegos/cjp1-2.php
@@ -123,12 +123,7 @@ if (isset($resultadoIntentos['intentos'])) {
 		});
 	</script>
 	<script>
-<<<<<<< HEAD
-		var segundos = 240;//240
-
-=======
-		var segundos = 5;
->>>>>>> 8ba5f193ec911bac613dd03573a53cb807fc660a
+		var segundos = 240;
 		let puntos = 0;
 
 		//Funcion que agrega el sonido al juego
