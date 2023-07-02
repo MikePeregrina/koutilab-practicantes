95aedb972361a64e44518b784d4c4e9dab1882f1
jaimereyesr <jaime.reyesr22@gmail.com>
2023-07-01 22:18:38 -0600

    Agregar cursos de informática a perfil primaria

--- Director-institucional/dashboard.php
+++ Director-institucional/dashboard.php
@@ -1,5 +1,9 @@
 <?php
-
+session_start();
+$id_user = $_SESSION['id_director'];
+if (empty($_SESSION['active']) || empty($_SESSION['id_director'])) {
+  header('location: ../../acciones/cerrarsesion.php');
+}
 include('../acciones/conexion.php');
 
 $user = mysqli_fetch_assoc(mysqli_query($conexion, "SELECT * FROM director_institucional WHERE id_director = '$id_user'"));
