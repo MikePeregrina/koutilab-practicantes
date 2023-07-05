69c481fcd04753ba1f9bc5d1bff1c234b0d4683f
MarSKZ <125574486+MarSKZ@users.noreply.github.com>
2023-07-04 20:56:29 -0600

    Actualización estadísticas de admin

--- admin/escuelas.php
+++ admin/escuelas.php
@@ -16,7 +16,6 @@ $fila = mysqli_fetch_assoc($result);
 
 <!DOCTYPE html>
 <html lang="en">
-<>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" href="img/lgk.png">
