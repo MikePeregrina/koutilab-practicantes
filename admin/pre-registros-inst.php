a8e3a8d4e252a15905364500c299a07a60be978e
jaimereyesr <jaime.reyesr22@gmail.com>
2023-08-04 00:21:10 -0600

    Preregistros

---
+++ admin/pre-registros-inst.php
@@ -0,0 +1,283 @@
+<?php
+session_start();
+$id_user = $_SESSION['id_admin'];
+if (empty($_SESSION['active']) || empty($_SESSION['id_admin'])) {
+    header('location: ../acciones/cerrarsesion.php');
+}
+include('../acciones/conexion.php');
+$user = mysqli_fetch_assoc(mysqli_query($conexion, "SELECT * FROM admin WHERE id_admin = $id_user"));
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="shortcut icon" href="img/lgk.png">
+<link rel="stylesheet" href="css/nav-barra.css">
+<link rel="stylesheet" href="css/administradores.css">
+<link rel="stylesheet" href="css/footer.css">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.4.1/css/responsive.bootstrap4.min.css">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.3/css/bulma.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/dataTables.bulma.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.bulma.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<title>KOUTILAB</title>
+</head>
+
+<body>
+
+    <!-- Header nav -->
+    <?php include 'header-nav.php'; ?>
+
+    <div class="containers">
+        <h1>PRE-REGISTROS INSTITUCIONAL</h1>
+    </div>
+
+
+    <section>
+
+        <div class="board p-2">
+            <table id="preregistros" width="100%" class="table border-top">
+                <thead>
+                    <tr>
+                        <td><b>Nombre</b></td>
+                        <td><b>Cargo</b></td>
+                        <td><b>WhatsApp</b></td>
+                        <td><b>Correo</b></td>
+                        <td><b>Escuela</b></td>
+                        <td><b>País</b></td>
+                        <td><b>Estado</b></td>
+                        <td><b>Grado</b></td>
+                        <td><b>Total alumnos</b></td>
+                        <td><b>Otro</b></td>
+                        <td><b>Acción</b></td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    include "../acciones/conexion.php";
+
+                    $query_formulario = mysqli_query($conexion, "SELECT * FROM formulario");
+                    $result = mysqli_num_rows($query_formulario);
+                    if ($result > 0) {
+                        while ($data = mysqli_fetch_assoc($query_formulario)) {
+
+                    ?>
+                            <tr>
+                                <td><?php echo $data['nombre_r']; ?></td>
+                                <td><?php echo $data['cargo']; ?></td>
+                                <td><?php echo $data['contacto']; ?></td>
+                                <td><?php echo $data['email']; ?></td>
+                                <td><?php echo $data['nombre_e']; ?></td>
+                                <td><?php echo $data['pais']; ?></td>
+                                <td><?php echo $data['estado']; ?></td>
+                                <td><?php echo $data['grado']; ?></td>
+                                <td><?php echo $data['numero_a']; ?></td>
+                                <td><?php echo $data['otro_c']; ?></td>
+                                <td>
+                                    <a href="acciones/preregistrar.php?id=<?php echo $data['id_formulario']; ?>" class="btn btn-success" style="margin-right: 5px;"><i class='fas fa-check'></i></a>
+                                    <form style="padding: 0px 0px;" action="acciones/eliminar_preregistro.php?id=<?php echo $data['id_formulario']; ?>" method="post" class="confirmar d-inline">
+                                        <button class="btn btn-danger" type="submit"><i class='fas fa-trash-alt'></i> </button>
+                                    </form>
+                                </td>
+                            </tr>
+                    <?php }
+                    } ?>
+                </tbody>
+            </table>
+        </div>
+
+    </section>
+
+
+    <?php include 'footer.php'; ?>
+
+    <script>
+        const btnAbrirModalA = document.querySelector("#btn-abrir-modalA");
+        const btnCerrarModalA = document.querySelector("#btn-cerrar-modalA");
+        const modalA = document.querySelector("#modalA");
+        btnAbrirModalA.addEventListener("click", () => {
+            modalA.showModal();
+        })
+
+        btnCerrarModalA.addEventListener("click", () => {
+            modalA.close();
+        })
+    </script>
+    <script>
+        const btnAbrirModalG = document.querySelector("#btn-abrir-modalG");
+        const btnCerrarModalG = document.querySelector("#btn-cerrar-modalG");
+        const modalG = document.querySelector("#modalG");
+        btnAbrirModalG.addEventListener("click", () => {
+            modalG.showModal();
+        })
+
+        btnCerrarModalG.addEventListener("click", () => {
+            modalG.close();
+        })
+    </script>
+    <script>
+        const btnAbrirModalE = document.querySelector("#btn-abrir-modalE");
+        const btnCerrarModalE = document.querySelector("#btn-cerrar-modalE");
+        const modalE = document.querySelector("#modalE");
+        btnAbrirModalE.addEventListener("click", () => {
+            modalE.showModal();
+        })
+
+        btnCerrarModalE.addEventListener("click", () => {
+            modalE.close();
+        })
+    </script>
+    <script>
+        const btnAbrirModalL = document.querySelector("#btn-abrir-modalL");
+        const btnCerrarModalL = document.querySelector("#btn-cerrar-modalL");
+        const modalL = document.querySelector("#modalL");
+        btnAbrirModalL.addEventListener("click", () => {
+            modalE.showModal();
+        })
+
+        btnCerrarModalL.addEventListener("click", () => {
+            modalL.close();
+        })
+    </script>
+    <script>
+        const btnAbrirModalList = document.querySelector("#btn-abrir-modalList");
+        const btnCerrarModalList = document.querySelector("#btn-cerrar-modalList");
+        const modalList = document.querySelector("#modalList");
+        btnAbrirModalList.addEventListener("click", () => {
+            modalList.showModal();
+        })
+
+        btnCerrarModalList.addEventListener("click", () => {
+            modalList.close();
+        })
+    </script>
+    <script>
+        const btn = document.querySelector('#menu-btn');
+        const menu = document.querySelector('#sidemenu');
+        btn.addEventListener('click', e => {
+            menu.classList.toggle("menu-expanded");
+            menu.classList.toggle("menu-collapsed");
+
+            document.querySelector('body').classList.toggle('body-expanded');
+        });
+    </script>
+    <script>
+        function disableIE() {
+            if (document.all) {
+                return false;
+            }
+        }
+
+        function disableNS(e) {
+            if (document.layers || (document.getElementById && !document.all)) {
+                if (e.which == 2 || e.which == 3) {
+                    return false;
+                }
+            }
+        }
+        if (document.layers) {
+            document.captureEvents(Event.MOUSEDOWN);
+            document.onmousedown = disableNS;
+        } else {
+            document.onmouseup = disableNS;
+            document.oncontextmenu = disableIE;
+        }
+        document.oncontextmenu = new Function("return false");
+    </script>
+    <script>
+        onkeydown = e => {
+            let tecla = e.which || e.keyCode;
+
+            // Evaluar si se ha presionado la tecla Ctrl:
+            if (e.ctrlKey) {
+                // Evitar el comportamiento por defecto del nevagador:
+                e.preventDefault();
+                e.stopPropagation();
+
+                // Mostrar el resultado de la combinación de las teclas:
+                if (tecla === 85)
+                    console.log("Ha presionado las teclas Ctrl + U");
+
+                if (tecla === 83)
+                    console.log("Ha presionado las teclas Ctrl + S");
+            }
+        }
+    </script>
+
+    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+    <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.5/js/dataTables.bulma.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.bulma.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.colVis.min.js"></script>
+
+    <script src="https://cdn.datatables.net/responsive/2.4.1/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.4.1/js/responsive.bootstrap4.min.js"></script>
+    <script>
+    $(document).ready(function() {
+      var table = $('#preregistros').DataTable({
+        responsive: true,
+        lengthMenu: [10, 25, 50, 100], // Opciones para mostrar diferentes cantidades de registros por página
+        pageLength: 10, // Cantidad de registros por página por defecto
+        ordering: true, // Habilitar la ordenación de columnas
+        language: {
+          "lengthMenu": "Mostrar _MENU_ registros por página",
+          "zeroRecords": "No se encontraron registros",
+          "info": "Mostrando _START_ al _END_ de _TOTAL_ registros",
+          "infoEmpty": "No hay registros disponibles",
+          "infoFiltered": "(filtrados de _MAX_ registros totales)",
+          "search": "Buscar:",
+          "paginate": {
+            "first": "Primero",
+            "last": "Último",
+            "next": "Siguiente",
+            "previous": "Anterior"
+          }
+        },
+        dom: 'Blfrtip', // Mostrar solo botones específicos en la parte superior derecha de la tabla
+        buttons: [
+          {
+            extend: 'pdfHtml5',
+            split:['excelHtml5', 'print'],
+            orientation: 'portrait', // Orientación del PDF (portrait o landscape)
+            pageSize: 'A4', // Tamaño del papel del PDF
+            exportOptions: {
+              columns: ':visible' // Exportar solo las columnas visibles
+            },
+            customize: function(doc) {
+              // Personalizar el estilo del PDF aquí usando la librería pdfmake
+              doc.content[1].table.body.forEach(row => row.forEach(cell => cell.alignment = 'center'));
+              doc.styles.tableHeader.fillColor = '#f8f8f8';
+              doc.styles.tableBodyEven.fillColor = '#f0f0f0';
+              doc.styles.tableBodyOdd.fillColor = '#ffffff';
+              doc.styles.tableHeader.color = '#000000';
+
+              
+            }
+          },
+        ]
+      });
+      table.buttons().container().appendTo($('div.column.is-half', table.table().container()).eq(0));
+    });
+  </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script src="js/funciones.js"></script>
+
+</body>
+
+</html>
