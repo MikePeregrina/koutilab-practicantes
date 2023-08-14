4f860345deb720b756eed41d2f051cb40fb508ab
RafaelSoledad02 <wwrafasc1@gmail.com>
2023-08-14 17:23:12 -0600

    Ejemplo de aplicar estrellas a juegos premium

--- alumno/primaria/cursos/programacion-web/basico/capsulas/contenido/juegos/cp1/level-final.php
+++ alumno/primaria/cursos/programacion-web/basico/capsulas/contenido/juegos/cp1/level-final.php
@@ -248,7 +248,7 @@ if (empty($existe)) {
 								confirmButtonText: '¡Vamos!',
 							}).then((result) => {
 								if (result.isConfirmed) {
-									window.location.href = '../../../../../../../rutas/ruta-pw-b.php';
+									window.location.href = '../acciones/insertar_ep1.php?id_capsula=' + 1 + '&id_curso=' + 1 + '&estrellas=' + 15;
 								}
 							})
 							correcto.play(); //agregando sonido al juego completado
