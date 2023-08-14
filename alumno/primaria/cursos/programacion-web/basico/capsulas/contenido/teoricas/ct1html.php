48204f95e6612a639e48a31a6968b7ff273cee47
jaimereyesr <jaime.reyesr22@gmail.com>
2023-08-14 17:14:50 -0600

    Ejemplo de nuevos archivos

--- alumno/primaria/cursos/programacion-web/basico/capsulas/contenido/teoricas/ct1html.php
+++ alumno/primaria/cursos/programacion-web/basico/capsulas/contenido/teoricas/ct1html.php
@@ -98,7 +98,7 @@ if (isset($resultadoIntentos['intentos'])) {
                         <li style="background-image: url('../../img/html/T1/18.gif');"></li>
                         <li>
                             <div>
-                                <form class="forms" id="evaluar" method="POST" enctype="multipart/form-data" action="../../acciones/insertar_pd2.php">
+                                <form class="forms" id="evaluar" method="POST" enctype="multipart/form-data" action="../../acciones/insertar_teorica.php">
                                     <h2>Para poder avanzar, responde la siguiente pregunta.</h2>
                                     <h1>¿Qué son los elementos?</h1>
                                     <div class="container-question">
@@ -120,6 +120,7 @@ if (isset($resultadoIntentos['intentos'])) {
                                     <input type="hidden" name="permiso" value="2">
                                     <input type="hidden" name="teorico" value="10">
                                     <input type="hidden" name="id_curso" value="1">
+                                    <input type="hidden" name="redireccion" value="..contenido/teoricas/ct1html.php">
                                     <input type="hidden" name="validar" id="validar" value="incorrecto">
                                 </form>
                             </div>
@@ -129,10 +130,10 @@ if (isset($resultadoIntentos['intentos'])) {
         </div>
     </div>
     <footer class="footerimga">
-		<div class="imagen-footer">
-			<img src="../../img/benvenida.png" alt="No-image">
-		</div>
-	</footer>
+        <div class="imagen-footer">
+            <img src="../../img/benvenida.png" alt="No-image">
+        </div>
+    </footer>
     <script>
         window.addEventListener("load", function() {
             var form = document.querySelector("form");
