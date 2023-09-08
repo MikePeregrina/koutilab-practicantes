dd2c6c7205c3a68c917513bc610a1ae75e625486
MarSKZ <125574486+MarSKZ@users.noreply.github.com>
2023-09-07 22:12:54 -0600

    Actualización de ruta pw para niños

--- alumno/primaria/cursos/programacion-web-ninos/intermedio/capsulas/contenido/teoricas/ct7js.php
+++ alumno/primaria/cursos/programacion-web-ninos/intermedio/capsulas/contenido/teoricas/ct7js.php
@@ -47,6 +47,7 @@ if (isset($resultadoIntentos['intentos'])) {
     <link rel="shortcut icon" href="../../../../../../img/lgk.png">
     <link rel="stylesheet" href="../../css/capsula-teoriaa.css" />
     <link rel="stylesheet" href="../../css/carrusel.css" />
+    <link rel="stylesheet" href="./css/columnas-teoricas.css" /> <!-- Agregar css de columnas -->
     <script src="https://kit.fontawesome.com/53845e078c.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
     <link rel="stylesheet" href="https://cdn.plyr.io/3.7.2/plyr.css" />
@@ -84,16 +85,52 @@ if (isset($resultadoIntentos['intentos'])) {
                         <li>
                             <a itlist="itList_6" href="#"></a>
                         </li>
-                        <li>
-                            <a itlist="itList_7" href="#"></a>
-                        </li>
                     </ul>
                     <ul id="slider">
                         <li style="background-image: url('../../img/js/T5.5/167.gif'); z-index:0; opacity: 1;"></li>
                         <li style="background-image: url('../../img/js/T5.5/168.gif');"></li>
                         <li style="background-image: url('../../img/js/T5.5/169.gif');"></li>
-                        <li style="background-image: url('../../img/js/T5.5/170.gif');"></li>
-                        <li style="background-image: url('../../img/js/T5.5/171.gif');"></li>
+                        <li>
+                            <!-- Copiar de aqui -->
+                            <h4 class="titulo"><b>Selecciona una palabra de lado izquierdo y relacionala con una del
+                                    lado derecho</b></h4>
+                            <div class="columnas">
+                                <div class="container-all">
+                                    <!-- Columna de lado izquierdo -->
+                                    <div class="left-column">
+                                        <!-- opciones estas son las principales -->
+                                        <div class="word-box" id="css">CSS</div>
+                                        <div class="word-box" id="sql">SQL</div>
+                                        <div class="word-box" id="html">HTML</div>
+                                        <div class="word-box" id="javascript">JavaScript</div>
+                                        <div class="word-box" id="php">PHP</div>
+                                    </div>
+                                    <!-- Mapeo donde se trazan las lineas -->
+                                    <canvas id="canvas"> </canvas>
+
+                                    <!-- columna de lado derecho -->
+                                    <div class="right-column">
+                                        <!-- Respuestas -->
+                                        <div class="word-box" id="interactividad"
+                                            onclick="checkAnswer('interactividad')">
+                                            Interactividad</div>
+                                        <div class="word-box" id="funcionalidad" onclick="checkAnswer('funcionalidad')">
+                                            Funcionalidad</div>
+                                        <div class="word-box" id="estructura" onclick="checkAnswer('estructura')">
+                                            Estructura
+                                        </div>
+                                        <div class="word-box" id="estilos" onclick="checkAnswer('estilos')">Estilos
+                                        </div>
+                                        <div class="word-box" id="administrar" onclick="checkAnswer('administrar')">
+                                            Administrar</div>
+                                    </div>
+                                </div>
+
+                                <!-- boton de verificar respuestas -->
+                                <button class="verificar">Comprobar respuestas</button>
+                            </div>
+                            <!-- Hasta aqui -->
+                        </li>
                         <li style="background-image: url('../../img/js/T5.5/172.gif');"></li>
                         <li>
                             <div>
@@ -132,6 +169,207 @@ if (isset($resultadoIntentos['intentos'])) {
 			<img src="../../img/benvenida.png" alt="No-image">
 		</div>
 	</footer>
+    <!-- Copiar de aqui -->
+    <script>
+        //Apartado de canvas para trazar lineas
+
+        //variables para la medida del canvas
+        const ALTURA_CANVAS = 290,
+            ANCHURA_CANVAS = 535;
+
+        // Obtener el elemento del DOM
+        const canvas = document.querySelector("#canvas");
+        canvas.width = ANCHURA_CANVAS;
+        canvas.height = ALTURA_CANVAS;
+
+        // Del canvas, obtener el contexto para poder dibujar
+        const contexto = canvas.getContext("2d");
+
+
+
+
+        // Apartado para seleccinador para relacionar columas
+        const palabras = document.querySelectorAll('.word-box');
+
+        //variables a utilizar y contadores
+        let palabraseleccionada = null;
+        let respuestasCorrectas = 0;
+        let respuestasIncorrectas = 0;
+
+        // Agregar eventos de clic a las palabras
+        palabras.forEach(word => {
+            word.addEventListener('click', selectWord);
+        });
+
+        // Función para seleccionar una palabra
+        function selectWord() {
+            if (palabraseleccionada) {
+                // Si ya hay una palabra seleccionada, la deseleccionamos
+                palabraseleccionada.classList.remove('seleccionado');
+            }
+            palabraseleccionada = this;
+            if (
+                palabraseleccionada.id !== 'interactividad' &&
+                palabraseleccionada.id !== 'funcionalidad' &&
+                palabraseleccionada.id !== 'estructura' &&
+                palabraseleccionada.id !== 'estilos' &&
+                palabraseleccionada.id !== 'administrar'
+            ) {
+                palabraseleccionada.classList.add('seleccionado');
+            } else {
+                palabraseleccionada = null;
+            }
+        }
+
+        // Función para verificar la respuesta
+        function checkAnswer(respuesta) {
+            const idPalabraSeleccionada = palabraseleccionada.id;
+            const estadopalabra = document.getElementById(respuesta);
+
+            //validamos que ya haya seleccionado una palabra
+            if (palabraseleccionada) {
+                //aqui para cada relacion la validamos en caso de ser correcta se trazara la linea
+                if (respuesta === 'estilos' && idPalabraSeleccionada === 'css') {
+                    palabraseleccionada.classList.add('correcto');
+                    // Comenzar
+                    contexto.beginPath();
+                    // Grosor de línea
+                    contexto.lineWidth = 3;
+                    // Color de línea 
+                    contexto.strokeStyle = "#84c42c";
+                    // Comenzamos en 0, 0
+                    contexto.moveTo(0, 30);
+                    // Hacemos una línea hasta 48, 48
+                    contexto.lineTo(560, 210);
+                    contexto.stroke(); // "Guardar" cambios
+                    //sumamos al contador
+                    respuestasCorrectas++;
+                } else if (respuesta === 'estructura' && idPalabraSeleccionada === 'html') {
+                    palabraseleccionada.classList.add('correcto');
+                    contexto.beginPath();
+                    contexto.lineWidth = 3;
+                    contexto.strokeStyle = "#84c42c";
+                    contexto.moveTo(0, 145);
+                    contexto.lineTo(560, 145);
+                    contexto.stroke();
+                    respuestasCorrectas++;
+                }
+                else if (
+                    respuesta === 'interactividad' && idPalabraSeleccionada === 'javascript'
+                ) {
+                    palabraseleccionada.classList.add('correcto');
+                    contexto.beginPath();
+                    contexto.lineWidth = 3;
+                    contexto.strokeStyle = "#84c42c";
+                    contexto.moveTo(0, 205);
+                    contexto.lineTo(560, 20);
+                    contexto.stroke();
+                    respuestasCorrectas++;
+                } else if (
+                    respuesta === 'funcionalidad' && idPalabraSeleccionada === 'php'
+                ) {
+                    palabraseleccionada.classList.add('correcto');
+                    contexto.beginPath();
+                    contexto.lineWidth = 3;
+                    contexto.strokeStyle = "#84c42c";
+                    contexto.moveTo(0, 260);
+                    contexto.lineTo(560, 75);
+                    contexto.stroke();
+                    respuestasCorrectas++;
+
+
+                } else if (
+                    respuesta === 'administrar' && idPalabraSeleccionada === 'sql'
+                ) {
+                    palabraseleccionada.classList.add('correcto');
+                    contexto.beginPath();
+                    contexto.lineWidth = 3;
+                    contexto.strokeStyle = "#84c42c";
+                    contexto.moveTo(0, 95);
+                    contexto.lineTo(560, 270);
+                    contexto.stroke();
+                    respuestasCorrectas++;
+                } else {
+                    palabraseleccionada.classList.add('incorrecto');
+                    respuestasIncorrectas++;
+                }
+
+                //una vez seleccionada la desabilitamos
+                palabraseleccionada.classList.remove('seleccionado');
+                palabraseleccionada.classList.add('deshabilitado');
+                palabraseleccionada.removeEventListener('click', selectWord);
+                //limpiamos la palabra seleccionada
+                palabraseleccionada = null;
+                estadopalabra.classList.add('deshabilitado');
+                estadopalabra.removeEventListener('click', selectWord);
+            }
+        }
+
+
+
+
+        // Agregar evento de clic al botón de comprobar respuestas
+        const botonComprobar = document.querySelector('.verificar');
+        botonComprobar.addEventListener('click', mostrarResultados);
+
+        // Función para mostrar los resultados
+        function mostrarResultados() {
+            let todasSeleccionadas = true;
+
+            // Verificar si todas las opciones han sido seleccionadas
+            palabras.forEach(word => { //se recorre cada opción utilizando el método forEach en la lista palabras
+                if (!word.classList.contains('deshabilitado')) { // verifica si no tiene la clase deshabilitado
+                    todasSeleccionadas = false;
+                }
+            });
+            //validamos que ya se hizo intento de resolver todo el juego
+            if (todasSeleccionadas) {
+                if (respuestasCorrectas < 3) {
+                    Swal.fire({
+                        //estrucutra de la alerta
+                        title: '!Puedes seguir mejorado!',
+                        html: `Respuestas correctas: ${respuestasCorrectas}<br>Respuestas incorrectas: ${respuestasIncorrectas}`,
+                        imageUrl: 'img/loop.gif',
+                        imageHeight: 350,
+                        backdrop: `
+                    rgba(0,143,255,0.6)
+                    url("img/fondo.gif")`,
+                        confirmButtonColor: '#a14cd9',
+                        confirmButtonText: '¡Genial!',
+                    });
+                } else {
+                    //llamamos a la alerta
+                    Swal.fire({
+                        //estrucutra de la alerta
+                        title: 'Resultados',
+                        html: `Respuestas correctas: ${respuestasCorrectas}<br>Respuestas incorrectas: ${respuestasIncorrectas}`,
+                        imageUrl: 'img/Thumbs-Up.gif',
+                        imageHeight: 350,
+                        backdrop: `
+                    rgba(0,143,255,0.6)
+                    url("img/fondo.gif")`,
+                        confirmButtonColor: '#a14cd9',
+                        confirmButtonText: '¡Genial!',
+                    });
+                }
+            }
+            //en caso de que no se hayan seleccionado todas mandamos alerta para notificar que se debe intentar relacionar todas las columnas
+            else {
+                Swal.fire({
+                    title: 'Oops...',
+                    text: 'Debes seleccionar todas las opciones antes de comprobar las respuestas.',
+                    imageUrl: 'img/loop.gif',
+                    imageHeight: 350,
+                    backdrop: `
+                rgba(0,143,255,0.6)
+                url("img/fondo.gif")`,
+                    confirmButtonColor: '#a14cd9',
+                    confirmButtonText: '¡Genial!',
+                });
+            }
+        }
+    </script>
+    <!-- Hasta aqui -->
     <script>
         window.addEventListener("load", function() {
             var form = document.querySelector("form");
