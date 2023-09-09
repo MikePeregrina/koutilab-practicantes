b88aaf3b5f7ee819b97ca285cbb52d7ed166eef2
MarSKZ <125574486+MarSKZ@users.noreply.github.com>
2023-09-08 21:56:04 -0600

    Actualización de contenido de cursos para niños

--- alumno/primaria/cursos/informatica-ninos/avanzado/capsulas/contenido/teoricas/ct20informatica.php
+++ alumno/primaria/cursos/informatica-ninos/avanzado/capsulas/contenido/teoricas/ct20informatica.php
@@ -406,7 +406,9 @@ if (isset($resultadoIntentos['intentos'])) {
     <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-        crossorigin="anon
+        crossorigin="anonymous"></script>
+    <!-- Hasta aqui -->
+    
     <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
     <script defer src="../../js/functions.js"></script>
 </body>
