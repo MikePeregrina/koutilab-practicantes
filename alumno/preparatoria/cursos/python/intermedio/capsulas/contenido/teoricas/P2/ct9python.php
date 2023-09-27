ff52022c54930054213b90082d342635b759b427
MarSKZ <125574486+MarSKZ@users.noreply.github.com>
2023-09-27 17:45:45 -0600

    Actualización de permisos pyI y pyA preparatoria

--- alumno/preparatoria/cursos/python/intermedio/capsulas/contenido/teoricas/P2/ct9python.php
+++ alumno/preparatoria/cursos/python/intermedio/capsulas/contenido/teoricas/P2/ct9python.php
@@ -6,7 +6,7 @@ if (empty($_SESSION['active']) || empty($_SESSION['id_alumno_preparatoria'])) {
 }
 include "../../../../../../../../../acciones/conexion.php";
 $id_user = $_SESSION['id_alumno_preparatoria'];
-$permiso = "capsula42";
+$permiso = "capsula57";
 $sql = mysqli_query($conexion, "SELECT c.*, d.* FROM capsulas_preparatoria c INNER JOIN detalle_capsulas_preparatoria d ON c.id_capsula = d.id_capsula WHERE d.id_alumno = $id_user AND c.nombre = '$permiso' AND d.id_curso = 5");
 $existe = mysqli_fetch_all($sql);
 if (empty($existe) && $id_user != 1) {
@@ -14,7 +14,7 @@ if (empty($existe) && $id_user != 1) {
 }
 //Verificar si ya se tiene permiso y no dar puntos de más
 //Verificar si permiso_intento es correcto
-$permiso_intento = 43;
+$permiso_intento = 58;
 $sql_permisos = mysqli_query($conexion, "SELECT * FROM detalle_capsulas_preparatoria WHERE id_capsula = $permiso_intento AND id_alumno = '$id_user' AND id_curso = 5");
 $result_sql_permisos = mysqli_num_rows($sql_permisos);
 //Script para poder ver cuantos intentos lleva el alumno en la capsula y mostrar cuantos puntos gano dependiendo los intentos
@@ -125,11 +125,11 @@ if (isset($resultadoIntentos['intentos'])) {
                                         <input type="checkbox" id="checkbox4" class="check-box" style="scale: 90%;">
                                         <label for="checkbox4">Son una forma de agrupar datos en estructuras de datos complejas, como matrices multidimensionales.</label>
                                     </div>
-                                    <input type="hidden" name="permiso" value="43">
+                                    <input type="hidden" name="permiso" value="58">
                                     <input type="hidden" name="teorico" value="10">
                                     <input type="hidden" name="id_curso" value="5">
                                     <input type="hidden" name="validar" id="validar" value="incorrecto">
-                                    <input type="hidden" name="redireccion" value="../contenido/teoricas/P2/ct7python.php">
+                                    <input type="hidden" name="redireccion" value="../contenido/teoricas/P2/ct9python.php">
                                 </form>
                             </div>
                         </li>
