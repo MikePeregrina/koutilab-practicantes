4ded60327580597e95819ed6135f7a509728ada1
jaimereyesr <jaime.reyesr22@gmail.com>
2023-11-07 21:27:50 -0600

    Re direccion login

--- index.php
+++ index.php
@@ -219,7 +219,7 @@ if (isset($_POST['iniciar_sesion'])) {
                 $sql_cont = "UPDATE `alumnos_secundaria` SET `conexiones`= '$cont'  WHERE id_alumno = '$id_usuario'";
                 $query_cont = mysqli_query($conexion, $sql_cont);
                 /*FIN NUEVA CONEXIÓN */
-                header('location: alumno-estructura/perfil.php');
+                header('location: alumno-estructura/secundaria/perfil.php');
             } else {
                 $alert = '<div style="color: red; margin-left: 80px;" class="alert alert-danger" role="alert">
                         Usuario o contraseña incorrecta
@@ -293,7 +293,7 @@ if (isset($_POST['iniciar_sesion'])) {
                 $sql_cont = "UPDATE `alumnos_preparatoria` SET `conexiones`= '$cont'  WHERE id_alumno = '$id_usuario'";
                 $query_cont = mysqli_query($conexion, $sql_cont);
                 /*FIN NUEVA CONEXIÓN */
-                header('location: alumno-estructura/perfil.php');
+                header('location: alumno-estructura/preparatoria/perfil.php');
             } else {
                 $alert = '<div style="color: red; margin-left: 80px;" class="alert alert-danger" role="alert">
                         Usuario o contraseña incorrecta
@@ -367,7 +367,7 @@ if (isset($_POST['iniciar_sesion'])) {
                 $sql_cont = "UPDATE `alumnos_universidad` SET `conexiones`= '$cont'  WHERE id_alumno = '$id_usuario'";
                 $query_cont = mysqli_query($conexion, $sql_cont);
                 /*FIN NUEVA CONEXIÓN */
-                header('location: alumno-estructura/perfil.php');
+                header('location: alumno-estructura/universidad/perfil.php');
             } else {
                 $alert = '<div style="color: red; margin-left: 80px;" class="alert alert-danger" role="alert">
                         Usuario o contraseña incorrecta
